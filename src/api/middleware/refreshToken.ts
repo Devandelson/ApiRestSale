5362d71ba3fdd73482e49e5d6d1a464052bafa40
Devandelson <devandelsongonzalez@gmail.com>
2025-09-04 21:18:50 -0400

    first commit

---
+++ src/api/middleware/refreshToken.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { tokenValid } from '../index.js';
+
+// Supongamos que los refresh tokens válidos están en DB:
+export default async function refreshTokens() {
+    const payloadID = process.env.payloadID;
+    const payloadname = process.env.payloadname;
+    const payloadPassword = process.env.payloadPassword;
+        
+    const payload = { id: payloadID, name: payloadname, password: payloadPassword};
+
+    // 3. Generar nuevos tokens
+    const newAccessToken = jwt.sign(
+        payload,
+        process.env.JWT_SECRET!,
+        { expiresIn: "15m" }
+    );
+
+    const newRefreshToken = jwt.sign(
+        payload,
+        process.env.JWT_SECRET!,
+        { expiresIn: "7d" }
+    );
+
+    // 4. colocando tokens nuevos a tokensValid
+    tokenValid.length = 0;
+    tokenValid.push(newAccessToken, newRefreshToken);
+}
